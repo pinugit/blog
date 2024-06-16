1dc1a4e297d4765c5a3b1e74897537f75f56546b
pinu <pinuwork@gmail.com>
2024-06-16 03:38:13 -0300

    initila commit

---
+++ db/migrate/20240615155305_create_blog_posts.rb
@@ -0,0 +1,10 @@
+class CreateBlogPosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :blog_posts do |t|
+      t.string :name
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
